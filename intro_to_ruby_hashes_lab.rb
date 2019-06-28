6989fa223c1484c4856a3361845d7c3a26cdc0a4
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-28 16:02:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 
 def my_hash_creator(key, value)
   my_hash_creator = {
-    :
+    :Grace Hopper => Grace Hopper
   }
   # return a hash that includes the key and value parameters passed into this method
 end
