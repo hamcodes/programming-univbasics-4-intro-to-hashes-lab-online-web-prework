fbc1b75a963daa4992d68d3cccd870ce4b9cea42
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-28 16:01:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+  my_hash_creator = {
+    :
+  }
   # return a hash that includes the key and value parameters passed into this method
 end
 
